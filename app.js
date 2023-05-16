d29439296f473721edf2e8e45d5346de2fa6686a
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-05-16 17:52:55 +0100

    name attributes given to input element in order to grab values using body-parser when form is submitted

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signUp.html");
 });
 
 app.post("/", (req, res) => {
-  res.send("labas");
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+
+  res.send("hi");
 });
 
 app.listen(3000, () => {
